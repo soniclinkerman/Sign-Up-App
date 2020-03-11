801074a4c2978205af87670857dc2ad47787c25d
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-03-11 10:26:26 -0400

    change all en to google

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ passport.deserializeUser(function(id, done){
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3004/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
