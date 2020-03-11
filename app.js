5cb9b487c999761d83e2ea42c920ed2df39d3330
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-03-11 10:14:14 -0400

    Add cookie session

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
-const session = require("express-session");
+const session = require("cookie-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
